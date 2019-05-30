6254f14dfef9432113921ed3d40545ea39d2b3e9
borispovod <boris@wings.ai>
2019-05-30 15:48:04 +0300

    described params with comments

--- x/poa/params.go
+++ x/poa/params.go
@@ -6,15 +6,18 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// New Paramstore for PoA module
 func NewKeyTable() params.KeyTable {
 	return params.NewKeyTable().RegisterParamSet(&types.Params{})
 }
 
+// Get max validators amount
 func (keeper Keeper) GetMaxValidators(ctx sdk.Context) (res uint16) {
 	keeper.paramStore.Get(ctx, types.KeyMaxValidators, &res)
 	return
 }
 
+// Get minimum validators amount
 func (keeper Keeper) GetMinValidators(ctx sdk.Context) (res uint16) {
 	keeper.paramStore.Get(ctx, types.KeyMinValidators, &res)
 	return
