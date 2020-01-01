d3613b8669f0cb90bb308abc0e7772e2863d29a8
borispovod <boris@wings.ai>
2020-01-01 19:02:10 +0200

    Module comments completed

--- x/poa/module.go
+++ x/poa/module.go
@@ -1,5 +1,4 @@
-//
-//
+// Implements PoA module.
 package poa
 
 import (
@@ -24,14 +23,17 @@ var (
 
 type AppModuleBasic struct{}
 
+// Module name.
 func (AppModuleBasic) Name() string {
 	return types.ModuleName
 }
 
+// Registering codecs.
 func (module AppModuleBasic) RegisterCodec(cdc *codec.Codec) {
 	RegisterCodec(cdc)
 }
 
+// Validate exists genesis.
 func (AppModuleBasic) ValidateGenesis(bz json.RawMessage) error {
 	var genesisState types.GenesisState
 	err := ModuleCdc.UnmarshalJSON(bz, &genesisState)
@@ -57,6 +59,7 @@ func (AppModuleBasic) ValidateGenesis(bz json.RawMessage) error {
 	return nil
 }
 
+// Generate default genesis.
 func (module AppModuleBasic) DefaultGenesis() json.RawMessage {
 	return ModuleCdc.MustMarshalJSON(types.GenesisState{
 		Parameters:    types.DefaultParams(),
@@ -64,23 +67,28 @@ func (module AppModuleBasic) DefaultGenesis() json.RawMessage {
 	})
 }
 
+// Register REST routes.
 func (AppModuleBasic) RegisterRESTRoutes(ctx context.CLIContext, r *mux.Router) {
 	rest.RegisterRoutes(ctx, r)
 }
 
+// Get transaction commands for CLI.
 func (AppModuleBasic) GetTxCmd(cdc *codec.Codec) *cobra.Command {
 	return client.GetTxCmd(cdc)
 }
 
+// Get query commands for CLI.
 func (AppModuleBasic) GetQueryCmd(cdc *codec.Codec) *cobra.Command {
 	return client.GetQueryCmd(cdc)
 }
 
+// PoA module.
 type AppModule struct {
 	AppModuleBasic
 	poaKeeper Keeper
 }
 
+// Create new PoA module.
 func NewAppModule(poaKeeper Keeper) AppModule {
 	return AppModule{
 		AppModuleBasic: AppModuleBasic{},
@@ -88,30 +96,40 @@ func NewAppModule(poaKeeper Keeper) AppModule {
 	}
 }
 
+// Get name of module.
 func (AppModule) Name() string {
 	return types.ModuleName
 }
 
+// Register dependencies of mo0dule.
 func (AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
 
+// Base route of module (for handler).
 func (AppModule) Route() string { return types.RouterKey }
 
+// Create new handler.
 func (app AppModule) NewHandler() sdk.Handler { return NewHandler(app.poaKeeper) }
 
+// Create new multisignature handler.
 func (app AppModule) NewMsHandler() msTypes.MsHandler { return NewMsHandler(app.poaKeeper) }
 
+// Get route for querier.
 func (AppModule) QuerierRoute() string { return types.RouterKey }
 
+// Get new querier for PoA module.
 func (app AppModule) NewQuerierHandler() sdk.Querier {
 	return NewQuerier(app.poaKeeper)
 }
 
+// Process begin block (abci).
 func (AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
 
+// Process end block (abci).
 func (AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
 	return []abci.ValidatorUpdate{}
 }
 
+// Initialize genesis.
 func (app AppModule) InitGenesis(ctx sdk.Context, data json.RawMessage) []abci.ValidatorUpdate {
 	var genesisState types.GenesisState
 
@@ -122,6 +140,7 @@ func (app AppModule) InitGenesis(ctx sdk.Context, data json.RawMessage) []abci.V
 	return []abci.ValidatorUpdate{}
 }
 
+// Export genesis.
 func (app AppModule) ExportGenesis(ctx sdk.Context) json.RawMessage {
 	genesisState := app.poaKeeper.ExportGenesis(ctx)
 	return ModuleCdc.MustMarshalJSON(genesisState)
