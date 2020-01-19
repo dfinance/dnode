d804c6feeadd2480e755eedaafd649a93235d1c8
borispovod <boris@wings.ai>
2020-01-19 12:42:03 +0200

    correct handle message for deploy contract

--- x/vm/handler.go
+++ x/vm/handler.go
@@ -10,11 +10,19 @@ func NewHandler(keeper Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) sdk.Result {
 		switch msg := msg.(type) {
 		case MsgDeployContract:
-			return keeper.DeployContract(ctx, msg).Result()
+			return handleMsgDeploy(ctx, keeper, msg)
 
 		default:
 			errMsg := fmt.Sprintf("unrecognized vm msg type: %v", msg.Type())
 			return sdk.ErrUnknownRequest(errMsg).Result()
 		}
 	}
+}
+
+func handleMsgDeploy(ctx sdk.Context, keeper Keeper, msg MsgDeployContract) sdk.Result {
+	if err := keeper.DeployContract(ctx, msg); err != nil {
+		return err.Result()
+	}
+
+	return sdk.Result{}
 }
