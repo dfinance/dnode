35e0786c061f9b13864ef362cca084d71fd5f136
borispovod <boris@wings.ai>
2020-02-26 22:38:06 +0300

    lcs in the box, support for u128

--- x/vmauth/types.go
+++ x/vmauth/types.go
@@ -5,77 +5,20 @@ import (
 	"github.com/WingsDao/wings-blockchain/helpers"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/exported"
-	"github.com/the729/lcs"
-	"math/big"
-	"strconv"
 )
 
 const (
 	amountLength = 16
-	denomLength  = 15
 	resourceKey  = "016ee00e2d212d7676b19de9ce7a4b598a339ae2286ef6b378c0c348b3fd3221ed"
 )
 
-/*
-// coin struct
-struct Coin {
-	denom: bytearray,
-	value: u64,
-}
-
-resource T {
-	// all balances.
-	balances: Vector.T<Self.Coin>,
-}
-*/
 type WBCoin struct {
-	Denom []byte // coin denom
-	Value uint64 // coin value
+	Denom []byte  // coin denom
+	Value sdk.Int // coin value
 }
 
 type AccountResource struct {
 	Balances []WBCoin // coins
-}
-
-// Just convert little endian to big endian (because of Cosmos sdk usage of big endian).
-func LeToBe(bytes []byte) {
-	for i := 0; i < len(bytes)/2; i++ {
-		bytes[len(bytes)-i-1], bytes[i] = bytes[i], bytes[len(bytes)-i-1]
-	}
-}
-
-// Convert bytes slice to coin.
-func bytesToCoin(bytes []byte) sdk.Coin {
-	numBz := bytes[:amountLength]
-	LeToBe(numBz)
-
-	amount := &big.Int{}
-	amount.SetBytes(numBz)
-
-	denom := make([]byte, 0)
-	for i := amountLength; i < amountLength+denomLength; i++ {
-		if bytes[i] != 0 {
-			denom = append(denom, bytes[i])
-		} else {
-			break
-		}
-	}
-
-	return sdk.Coin{Amount: sdk.NewIntFromBigInt(amount), Denom: string(denom)}
-}
-
-// byte array to chunks.
-func split(buf []byte, lim int) [][]byte {
-	var chunk []byte
-	chunks := make([][]byte, 0, len(buf)/lim+1)
-	for len(buf) >= lim {
-		chunk, buf = buf[:lim], buf[lim:]
-		chunks = append(chunks, chunk)
-	}
-	if len(buf) > 0 {
-		chunks = append(chunks, buf[:len(buf)])
-	}
-	return chunks
 }
 
 // convert byte array to coins.
@@ -85,38 +28,11 @@ func bytesToCoins(coins []WBCoin) sdk.Coins {
 	} else {
 		realCoins := make(sdk.Coins, 0)
 		for _, coin := range coins {
-			str := strconv.FormatUint(coin.Value, 10)
-			amount, _ := sdk.NewIntFromString(str)
-			realCoins = append(realCoins, sdk.NewCoin(string(coin.Denom), amount))
+			realCoins = append(realCoins, sdk.NewCoin(string(coin.Denom), coin.Value))
 		}
 
 		return realCoins
 	}
-}
-
-// Convert coin to  bytes
-func coinToBytes(coin sdk.Coin) []byte {
-	// 128 bits (16 bytes) for balance
-	// 15 bytes for denom
-	// little endian for numbers
-	val := helpers.BigToBytes(coin.Amount, amountLength)
-	denom := make([]byte, denomLength)
-	for i, c := range coin.Denom {
-		denom[i] = byte(c)
-	}
-	return append(val, denom...)
-}
-
-// Convert coins to bytes.
-func coinsToBytes(coins sdk.Coins) []byte {
-	bytes := make([]byte, 0)
-	for _, coin := range coins {
-		if !coin.IsZero() {
-			bytes = append(bytes, coinToBytes(coin)...)
-		}
-	}
-
-	return bytes
 }
 
 // Bytes to libra compability.
@@ -133,6 +49,7 @@ func AddrToPathAddr(addr []byte) []byte {
 	return bytes
 }
 
+// Get resource path.
 func GetResPath() []byte {
 	data, err := hex.DecodeString(resourceKey)
 	if err != nil {
@@ -152,7 +69,7 @@ func AccResourceFromAccount(acc exported.Account) AccountResource {
 		for _, coin := range accCoins {
 			balances = append(balances, WBCoin{
 				Denom: []byte(coin.Denom),
-				Value: coin.Amount.BigInt().Uint64(),
+				Value: coin.Amount,
 			})
 		}
 
@@ -162,7 +79,7 @@ func AccResourceFromAccount(acc exported.Account) AccountResource {
 
 // Convert acc to bytes
 func AccToBytes(acc AccountResource) []byte {
-	bytes, err := lcs.Marshal(acc)
+	bytes, err := helpers.Marshal(acc)
 	if err != nil {
 		panic(err)
 	}
@@ -172,7 +89,7 @@ func AccToBytes(acc AccountResource) []byte {
 
 func BytesToAccRes(bz []byte) AccountResource {
 	var accRes AccountResource
-	err := lcs.Unmarshal(bz, &accRes)
+	err := helpers.Unmarshal(bz, &accRes)
 	if err != nil {
 		panic(err)
 	}
