17a09fbcb72273413aa71c46f36ed2dec6c1102a
borispovod <boris@wings.ai>
2020-04-15 06:19:17 +0300

    small refactoring of vm related modules

--- x/common_vm/common.go
+++ x/common_vm/common.go
@@ -1,33 +1,19 @@
 package common_vm
 
 import (
-	"bytes"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/dfinance/dvm-proto/go/vm_grpc"
 )
 
-var (
-	KeyDelimiter = []byte(":")
-	VMKey        = []byte("vm")
-)
-
 // Interface for other keepers to get/set data.
 type VMStorage interface {
+	// Access path for oracle.
 	GetOracleAccessPath(assetCode string) *vm_grpc.VMAccessPath
+
+	// Setters/getters.
 	SetValue(ctx sdk.Context, accessPath *vm_grpc.VMAccessPath, value []byte)
 	GetValue(ctx sdk.Context, accessPath *vm_grpc.VMAccessPath) []byte
-	DelValue(ctx sdk.Context, accessPath *vm_grpc.VMAccessPath)
-}
 
-// Make path for storage from VMAccessPath.
-func MakePathKey(path vm_grpc.VMAccessPath) []byte {
-	return bytes.Join(
-		[][]byte{
-			VMKey,
-			path.Address,
-			path.Path,
-		},
-		KeyDelimiter,
-	)
+	// Delete value in VM storage.
+	DelValue(ctx sdk.Context, accessPath *vm_grpc.VMAccessPath)
 }
