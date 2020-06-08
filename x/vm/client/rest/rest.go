7a44d602ca9c69c5ce40324aaccd7c6402b9cd70
Boris Povod <boris@wings.ai>
2020-06-08 16:46:50 +0300

    Showing tx id in stack trace (#134)
    * dncli new query support to get detailed vm error
    
    * vm errors to vm statuses, starting tests
    
    * tests
    
    * show tx id in stack trace, fixed tabs in stack trace
    
    * showing str status

--- x/vm/client/rest/rest.go
+++ x/vm/client/rest/rest.go
@@ -187,7 +187,7 @@ func getData(cliCtx context.CLIContext) http.HandlerFunc {
 // @Accept  json
 // @Produce json
 // @Param txHash path string true "transaction hash"
-// @Success 200 {object} VmData
+// @Success 200 {object} VmTxStatus
 // @Failure 422 {object} rest.ErrorResponse "Returned if the request doesn't have valid path params"
 // @Failure 500 {object} rest.ErrorResponse "Returned on server error"
 // @Router /vm/tx/{txHash} [get]
