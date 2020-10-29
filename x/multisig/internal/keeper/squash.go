6646b053ac6ad2975769dfd508a03fe44e995ab7
Mikhail Kornilov <mikhailkornilov@wings.ai>
2020-10-29 18:40:18 +0300

    [DFI-929] multisig mod: call queue fix for genesis squash; app export refactoring, debug added

--- x/multisig/internal/keeper/squash.go
+++ x/multisig/internal/keeper/squash.go
@@ -10,10 +10,13 @@ func (k Keeper) PrepareForZeroHeight(ctx sdk.Context) error {
 	// queue modifications resets call confirmation timeout
 	calls := k.getCalls(ctx)
 	for _, call := range calls {
-		// queue
-		k.RemoveCallFromQueue(ctx, call.ID, call.Height)
-		k.addCallToQueue(ctx, call.ID, 0)
-		// call
+		// add to the queue if call is not handled yet
+		if err := call.CanBeVoted(); err == nil {
+			k.RemoveCallFromQueue(ctx, call.ID, call.Height)
+			k.addCallToQueue(ctx, call.ID, 0)
+		}
+
+		// update call
 		call.Height = 0
 		k.StoreCall(ctx, call)
 	}
